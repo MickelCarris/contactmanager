bf6e5c56e4e3fd058aa5faff0d59f41d0d7f8cb8
MickelCarris <mickelcarris0007@gmail.com>
2019-08-03 16:58:33 +0530

    Updates

---
+++ precache-manifest.aef208fae9379cee81d071baf20ab9b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30685080420a7b45e03742783aae73b3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "61b8068627a75b1cf08e",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4f8d99d0331eba6628ee",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "61b8068627a75b1cf08e",
+    "url": "/contactmanager/static/js/2.d865a23f.chunk.js"
+  },
+  {
+    "revision": "4f8d99d0331eba6628ee",
+    "url": "/contactmanager/static/js/main.7ed9a4b1.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
